ee25f506228ef5b6d870d8b498df055093b6c144
Fernando Araujo <110340442+fasinnen@users.noreply.github.com>
2024-01-07 17:10:52 -0300

    Add the Transformation Engine - dbt project

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import os
 from generic_consumer import GenericConsumer
+from dbt.cli.main import dbtRunner, dbtRunnerResult
+
+def run_dbt():
+    os.chdir('transformation_engine')
+    dbt = dbtRunner()
+
+    cli_args = ["run"]
+    res: dbtRunnerResult = dbt.invoke(cli_args)
+    
+    for r in res.result:
+        print(f"{r.node.name}: {r.status}")
 
 if __name__ == '__main__':
     consumer = GenericConsumer()
+
     consumer.ingest()
+    run_dbt()
